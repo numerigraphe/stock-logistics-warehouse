512fc5a130016613431c159b78d6048c7be2afdd
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-08-27 16:03:18 +0200

    move the module to the root of the directory

---
+++ stock_orderpoint_creator/__openerp__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher, Matthieu Dietrich (Camptocamp)
+#    Copyright 2012-2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Order point creator',
+ 'summary': 'Configuration of order point in mass',
+ 'version': '1.0',
+ 'author': 'Camptocamp',
+ 'category': 'Warehouse',
+ 'license': 'AGPL-3',
+ 'complexity': 'easy', #easy, normal, expert
+ 'website': "http://www.camptocamp.com",
+ 'depends': ['procurement'],
+ 'description': """
+Order point creator
+===================
+
+Add a wizard to configure massively order points for multiple product
+
+Contributors
+------------
+
+ * Yannick Vaucher <yannick.vaucher@camptocamp.com>
+ * Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
+
+""",
+ 'website': 'http://www.openerp.com',
+ 'demo': [],
+ 'data': ["wizard/orderpoint_creator_view.xml",
+          "security/ir.model.access.csv",
+          ],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
